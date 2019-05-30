3e9f846c5f74db24b76de02cbe60014b88a211b7
Alex Polywka <apolywka@cs.washington.edu>
2019-05-30 14:40:38 -0700

    Added donate button and stuttering fix tip and reduced permission requirements.

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({speed: "1.0"}, function() {
-  });
   
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
