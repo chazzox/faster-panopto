d5bec23f2f1e7b27011a391fc5165768f0eab3cf
Alex Polywka <Rellikolbaid@users.noreply.github.com>
2019-05-31 01:12:28 -0700

    Interface update, UX improvements, local value storage added

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 
 chrome.runtime.onInstalled.addListener(function() {
+  // Setup local storage
+  chrome.storage.local.set({"speed": 1.0}, function(){});
   
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
+      // Condition to only enable the extension on panopto sites
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostContains: '.panopto'},
       })],
--- popup.js
+++ popup.js
@@ -1,15 +1,33 @@
+// UI references
 let changeSpeed = document.getElementById('changeSpeed');
 let videoPlayer1 = document.getElementsByTagName("video")[0];
 let videoPlayer2 = document.getElementsByTagName("video")[1];
-let playerSpeed = document.getElementById('speedVal');
+let playerSpeedField = document.getElementById('speedValField');
 
-playerSpeed.setAttribute("autocomplete", "off");
+chrome.storage.local.get('speed', function(result){
+    console.log("Got speed, was : " + result.speed);
+    playerSpeedField.value = result.speed;
+});
 
 
+
+// Autocomplete is really annoying because it covers up the button
+playerSpeedField.setAttribute("autocomplete", "off");
+
+// Disable enter key on the field
+playerSpeedField.addEventListener('submit', function(event) {
+    event.preventDefault();
+}, false);
+
+// Handles button click logic. 
 changeSpeed.onclick = function(element) {
-    console.log(playerSpeed);
+    var fieldVal = playerSpeedField.value;
+
+    chrome.storage.local.set({'speed': fieldVal}, function(){});
+
+    // Gets the tab context to execute scripts in.
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id, {code: 'document.getElementsByTagName("video")[0].playbackRate = ' + playerSpeed.value});
-        chrome.tabs.executeScript(tabs[0].id, {code: 'document.getElementsByTagName("video")[1].playbackRate = ' + playerSpeed.value});
+        chrome.tabs.executeScript(tabs[0].id, {code: 'document.getElementsByTagName("video")[0].playbackRate = ' + fieldVal});
+        chrome.tabs.executeScript(tabs[0].id, {code: 'document.getElementsByTagName("video")[1].playbackRate = ' + fieldVal});
     });
 }
