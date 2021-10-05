443775027444983e68c74d9e80aefe8da8c05151
chazzox <42206116+chazzox@users.noreply.github.com>
2021-10-05 15:56:00 +0100

    new styles

--- source/index.js
+++ source/index.js
@@ -1,32 +1,76 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import browser, { storage } from 'webextension-polyfill';
+import browser from 'webextension-polyfill';
 import styled, { createGlobalStyle } from 'styled-components';
 
 const Global = createGlobalStyle`
 	* {
-		font-family: 'Fira Code', monospace;
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+			Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+
+		color: white;
 	}
 	body {
 		margin: 0;
+		background-color: #2c2c2c;
+		padding: 5px;
+	}
+	span,
+	button,
+	a {
+		font-weight: 500;
 	}
 `;
-
-const Row = styled.div``;
 
 const Title = styled.h1`
 	margin: 0px 25px;
 	white-space: nowrap;
+	text-align: center;
+	font-size: 20px;
 `;
 
-const Footer = styled.div``;
+const Row = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 10px 0px;
+`;
 
 const Button = styled.button`
-	display: inline-block;
+	font-size: 15px;
+	white-space: nowrap;
+	margin: 0px 10px;
+	padding: 5px;
+	color: black;
+	background: white;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+	transition: background-color ease 0.2s;
+	transition: color ease 0.2s;
+	&:hover {
+		background-color: #2f884a;
+		color: white;
+	}
 `;
 
 const Input = styled.input`
 	display: inline-block;
+	width: 100px;
+	margin: 0px 10px;
+	color: black;
+	&:focus {
+		outline: none;
+	}
+`;
+
+const Footer = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+`;
+
+const Link = styled(Button)`
+	text-decoration: none;
 `;
 
 function App() {
@@ -79,16 +123,30 @@ function App() {
 						<Button onClick={setVideoSpeed}>Set Speed</Button>
 					</Row>
 					<Row>
-						download <Button>save as</Button>
+						<span>Download</span> <Button>Save as...</Button>
 					</Row>
 				</>
 			) : (
-				<>disabled</>
+				<Title>Disabled</Title>
 			)}
-			<Footer>links</Footer>
+			<Footer>
+				<Github />
+				<Twitter />
+			</Footer>
 		</>
 	);
 }
+
+const Github = () => (
+	<Link as="a" target="_blank" href="https://github.com/chazzox/">
+		github
+	</Link>
+);
+const Twitter = () => (
+	<Link as="a" target="_blank" href="https://twitter.com/_chazzox_">
+		twitter
+	</Link>
+);
 
 const app = document.getElementById('app');
 
