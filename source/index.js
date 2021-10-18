df231eab701edafb580a9d2984fbd579e7b27883
PINPAL <18197217+PINPAL@users.noreply.github.com>
2021-10-18 06:07:30 +0100

    make it look nice

--- source/index.js
+++ source/index.js
@@ -7,18 +7,113 @@ const Global = createGlobalStyle`
 	* {
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
 			Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
-
 		color: white;
 	}
 	body {
+		min-height: 120px;
+		min-width: 180px;
 		margin: 0;
-		background-color: #2c2c2c;
+		background: linear-gradient(45deg, rgb(13, 25, 51) 40%, rgb(10, 16, 21));
 		padding: 5px;
 	}
 	span,
 	button,
 	a {
 		font-weight: 500;
+	}
+
+	/* Hide + and - buttons on input */
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+	-webkit-appearance: none;
+	margin: 0;
+	}
+	input[type=number] {
+	-moz-appearance: textfield;
+	}
+
+	#errorContainer {
+		background: #282c2f;
+		border: none;
+		padding-right: 12px;
+		padding-left: 2px;
+		padding-top: 2px;
+		padding-bottom: 2px;
+		margin-left: 8px;
+		margin-right: 8px;
+		border-radius: 9px;
+
+		h1 {
+			font-weight: 600;
+			width: 100%;
+			box-sizing: border-box;
+			margin: 0;
+			margin-top: 12px;
+			margin-bottom: 12px;
+			padding: 8px;
+			border-left-style: solid;
+			border-width: 4px;
+			border-color: #de105c;
+		}
+	}
+	#wrapper {
+		width: 100%;
+		border-style: solid;
+		border-width: 2px;
+		border-color: rgb(65, 67, 78);
+		background-color: rgb(0, 4, 8);
+		padding: 6px;
+		border-radius: 18px;
+
+		#speedContainer {
+			background: rgb(0, 4, 8);
+			border-radius: 9px;
+			height: 80px;
+			max-height: 80px;
+			overflow: hidden;
+			position: relative;
+			padding-right: 30px;
+
+			button {
+				position: absolute;
+				right: 0;
+				margin: 0;
+				display: inline-block;
+				background-color: rgb(70, 74, 77);
+				outline: none;
+				border: none;
+				height: 25px;
+				width: 30px;
+				background-repeat: no-repeat;
+				background-size: 9px;
+				background-position: center;
+
+				&#applySpeed {
+					bottom: 0;
+					left: 0;
+					width: 100%;
+					height: 30px;
+					max-height: 30px;
+					line-height: 30px;
+					padding: 0;
+					border-radius: 0;
+					background-color: rgba(70, 74, 77, 0.85);
+				}
+
+				&#upArrow {
+					top: 0;
+					background-image: url("/images/up.png")
+				}
+				&#downArrow {
+					bottom: 30px;
+					background-image: url("/images/down.png")
+				}
+
+				&:hover {
+					background-color: rgb(100, 104, 107) !important;
+				}
+			}
+		}
 	}
 `;
 
@@ -37,28 +132,39 @@ const Row = styled.div`
 `;
 
 const Button = styled.button`
+	text-transform: capitalize;
 	font-size: 13px;
 	white-space: nowrap;
 	margin: 0px 5px;
-	padding: 4px;
-	color: black;
-	background: white;
+	padding-top: 6px;
+	padding-bottom: 6px;
+	padding-left: 12px;
+	padding-right: 12px;
+	color: rgba(255, 255, 255, 0.9);
+	background: rgba(98, 103, 111, 0.5);
 	border: none;
-	border-radius: 5px;
+	border-radius: 100px;
 	cursor: pointer;
-	transition: background-color ease 0.2s;
-	transition: color ease 0.2s;
+	transition: background-color 0.25s ease;
 	&:hover {
-		background-color: #2f884a;
+		background: rgba(98, 103, 111, 1);
 		color: white;
 	}
 `;
 
 const Input = styled.input`
+	background: #282c2f;
+	border: none;
 	display: inline-block;
-	width: 100px;
-	margin: 0px 10px;
-	color: black;
+	width: 100%;
+	color: white;
+	padding-top: 8px;
+	padding-bottom: 8px;
+	padding-left: 24px;
+	padding-right: 12px;
+	height: 50px;
+	max-height: 50px;
+	box-sizing: border-box;
 	&:focus {
 		outline: none;
 	}
@@ -67,6 +173,8 @@ const Input = styled.input`
 const Footer = styled.div`
 	display: flex;
 	justify-content: space-evenly;
+	margin-top: 8px;
+	margin-bottom: 2px;
 `;
 
 const Link = styled(Button)`
@@ -106,30 +214,46 @@ function App() {
 		<>
 			<Global />
 			<Title>faster-panopto</Title>
-			{enabled ? (
-				<>
-					<Row>
-						<Input
-							step="0.1"
-							type="number"
-							onChange={(e) => {
-								browser.storage.local.set({
-									speed: e.target.value
-								});
-								setSpeed(e.target.value);
-							}}
-							value={speed}
-						/>
-						<Button onClick={setVideoSpeed}>Set Speed</Button>
-					</Row>
-				</>
-			) : (
-				<Title>Disabled</Title>
-			)}
-			<Footer>
-				<Github />
-				<Twitter />
-			</Footer>
+			<Row>
+				<div id="wrapper">
+					{enabled ? (
+						<>
+							<div id="speedContainer">
+								<Input
+									step="0.1"
+									type="number"
+									onChange={(e) => {
+										browser.storage.local.set({
+											speed: e.target.value
+										});
+										setSpeed(parseInt(e.target.value));
+									}}
+									value={speed}
+								/>
+								<button
+									id="upArrow"
+									onClick={() => setSpeed(speed + 0.1)}
+								></button>
+								<button
+									id="downArrow"
+									onClick={() => setSpeed(speed - 0.1)}
+								></button>
+								<Button id="applySpeed" onClick={setVideoSpeed}>
+									Apply Speed
+								</Button>
+							</div>
+						</>
+					) : (
+						<div id="errorContainer">
+							<Title>Not available on this page!</Title>
+						</div>
+					)}
+					<Footer>
+						<Github />
+						<Twitter />
+					</Footer>
+				</div>
+			</Row>
 		</>
 	);
 }
